434438339df8669724c26c4c0256ac914d410e16
Ctrl-Tim <timon.istiukov@mail.ru>
2021-12-22 03:19:53 +0400

    commit 1

--- src/HangarPanel.java
+++ src/HangarPanel.java
@@ -2,15 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class HangarPanel extends JPanel {
-    private final Hangar<Plane, IMissilesForm> hangar;
+    private final HangarCollection hangarCollection;
+    private String selectedItem = null;
 
+    @Override
     public void paint(Graphics g) {
-        if (hangar != null) {
-            hangar.Draw(g);
+        if (selectedItem != null) {
+            if (hangarCollection != null) {
+                hangarCollection.get(selectedItem).Draw(g);
+            }
         }
     }
 
-    public HangarPanel(Hangar<Plane, IMissilesForm> hangar) {
-        this.hangar = hangar;
+    public void setSelectedItem(String selectedItem) {
+        this.selectedItem = selectedItem;
+    }
+
+    public HangarPanel(HangarCollection hangarCollection) {
+        this.hangarCollection = hangarCollection;
     }
 }
